fdacfc5cbe1f1007dadbe45b83f044fb9a1859b7
Hisami Kurita <hisahisa1996@gmail.com>
2021-09-15 01:39:30 +0900

    update

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -95,8 +95,8 @@ export default {
     return {
       TextSegmentDelay01: 0,
       TextSegmentDelay02: 0.176,
-      TextSegmentDelay03: 0.504,
-      TextSegmentDelay04: 0.64,
+      TextSegmentDelay03: 0.48,
+      TextSegmentDelay04: 0.504,
     }
   },
   mounted() {
