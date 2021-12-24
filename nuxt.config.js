146f3194a65fa6c0296a9e5ba59c8390ef32f7c8
栗田久海 <kurita_hisami@liginc.co.jp>
2021-12-24 23:35:42 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,8 +68,4 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
-
-  generate: {
-    fallback: true,
-  }
 }
--- plugins/asscroll-and-gsap.client.js
+++ plugins/asscroll-and-gsap.client.js
@@ -6,43 +6,42 @@ import { Draggable } from '../vendor/Draggable'
 import { InertiaPlugin } from '../vendor/InertiaPlugin'
 gsap.registerPlugin(CustomEase, Draggable, InertiaPlugin, ScrollTrigger);
 
+const asscroll = new ASScroll({
+  ease: 0.09,
+  customScrollbar: false,
+});
+
 const easing = {
   transform: CustomEase.create('transform', 'M0,0 C0.44,0.05 0.17,1 1,1'),
   transformReverse: CustomEase.create('transformReverse', 'M0,0 C0,0.852 0.87,0.542 1,1'),
   colorAndOpacity: CustomEase.create('colorAndOpacity', 'M0,0 C0.26,0.16 0.1,1 1,1 '),
 }
 
-// ScrollTrigger.defaults({
-//   scroller: asscroll.containerElement
-// })
-// ScrollTrigger.scrollerProxy(asscroll.containerElement, {
-//   scrollTop(value) {
-//     if (arguments.length) {
-//       asscroll.currentPos = value;
-//       return;
-//     }
-//     return asscroll.currentPos;
-//   },
-//   getBoundingClientRect() {
-//     return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
-//   },
-// });
+ScrollTrigger.defaults({
+  scroller: asscroll.containerElement
+})
+ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+  scrollTop(value) {
+    if (arguments.length) {
+      asscroll.currentPos = value;
+      return;
+    }
+    return asscroll.currentPos;
+  },
+  getBoundingClientRect() {
+    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
+  },
+});
 
 function render() {
   window.requestAnimationFrame(render)
   ScrollTrigger.update()
 }
 render();
-// ScrollTrigger.addEventListener("refresh", asscroll.resize);
+ScrollTrigger.addEventListener("refresh", asscroll.resize);
 
 export default (context, inject) => {
-  // setTimeout(() => {
-    const asscroll = new ASScroll({
-      ease: 0.09,
-      customScrollbar: false,
-    });
-    inject('asscroll', asscroll);
-  // }, 100)
+  inject('asscroll', asscroll);
   inject('gsap', gsap);
   inject('ScrollTrigger', ScrollTrigger);
   inject('Draggable', Draggable);
@@ -53,7 +52,7 @@ export default (context, inject) => {
  * ロード時に再度有効化
  * https://github.com/hisamikurita/hisamikurita-portfoliosite/issues/11
  */
-// window.addEventListener('load', () => {
-//   asscroll.disable();
-//   asscroll.enable();
-// });
+window.addEventListener('load', () => {
+  asscroll.disable();
+  asscroll.enable();
+});
