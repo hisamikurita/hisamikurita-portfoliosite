a78156775bafa326eb8d91455617071e82ea93de
Hisami Kurita <hisahisa1996@gmail.com>
2022-03-06 16:59:58 +0900

    feat: test

--- pages/works/_slug.vue
+++ pages/works/_slug.vue
@@ -2,6 +2,11 @@
   <div class="works">
     <div
       class="works-bg"
+      :style="`
+      background-color:${currentProject.siteColor.bodyContentsColor};
+      color:${currentProject.siteColor.allTextColor};
+      border-color:${currentProject.siteColor.allTextColor};
+      `"
     >
       <WorksMainVisualSection :current-project="currentProject" />
       <WorksProjectVideoSection :current-project="currentProject" />
