a37a791a07bbc0c5a543c4a452029bc6e2875750
Hisami Kurita <hisahisa1996@gmail.com>
2022-03-15 01:52:21 +0900

    fix: ogppath

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://hisamikurita.com/',
+        content: 'https://hsmkrt1996.com/',
       },
       {
         hid: 'og:title',
